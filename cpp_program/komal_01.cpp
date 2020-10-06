038cd1a0e943f602453af2f062c9a2b5bbd9f7ee
komal swami <46648301+komalswami@users.noreply.github.com>
2020-10-06 22:07:34 +0530

    Create komal_01.cpp

---
+++ cpp_program/komal_01.cpp
@@ -0,0 +1,19 @@
+/*       
+          Name: komal swami
+          Program Name : Find ASCII Value of Character                      
+*/
+
+/*###########################Program Name################################## */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
+/*###########################Program End################################## */
