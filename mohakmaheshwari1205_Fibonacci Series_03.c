7fb375d7dd9b0146635ba055326b57eda8b19eaf
mohakmaheshwari1205 <68627419+mohakmaheshwari1205@users.noreply.github.com>
2020-10-07 20:04:18 +0530

    Add files via upload

---
+++ mohakmaheshwari1205_Fibonacci Series_03.c
@@ -0,0 +1,25 @@
+/*       
+          Name: Mohak Maheshwari
+          Program Name : Fibonacci Series Program   (as per list)                           
+*/
+
+/*###########################Program start################################## */
+
+#include <stdio.h>
+
+int main()
+{
+    int a=0,b=1,c,i,n;    
+ printf("Enter the number of elements: ");    
+ scanf("%d",&n);   
+ printf("%d %d ",a,b); //printing 0 and 1    
+ for(i=2;i<n;i++) //loop starts from 2 because 0 and 1 are already printed    
+ {    
+  c=a+b;;    
+  printf("%d ",c);  
+  a=b;    
+  b=c;    
+ }    
+   return 0;  
+}
+/*###########################Program End################################## */
