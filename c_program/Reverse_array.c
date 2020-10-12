571e3afe98b20abc2b4ec891525b0a6c79361ae8
hariom09090 <hariom24568@gmail.com>
2020-10-13 00:39:15 +0530

    modify Reverse an array

--- c_program/Reverse_array.c
+++ c_program/Reverse_array.c
@@ -1,28 +1,35 @@
 /*       
-          Name: Amit Sagar
+          Name: HARI OM
           Program Name : Reverse an array                         
 */
 
 /*###########################Program start################################## */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-int n;
-scanf("%d",&n);
-int arr[n];
-int i;
-for(i = 0; i < n; i++)
-{
-scanf("%d",&arr[i]);
-}
-printf("Reversed array is:\n");
-for(i = n-1; i >= 0; i--)
-{
-printf("%d\n",arr[i]);
-}
-return 0;
+    int n, i, temp;
+    printf("Enter the total number : ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter number :\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+    printf("Reversed array is:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
 }
 
 /*###########################Program End################################## */
