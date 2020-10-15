804bac835dc5e938b4c17066f5f487e5b5dfa307
Manav Bansal <67185463+manavbansalcoder@users.noreply.github.com>
2020-10-15 21:02:53 +0530

    C# codes

---
+++ armstrong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_18_09_2020
+{
+    class armstrong
+    {
+		static void Main(string[] agrs)
+		{
+			int r, s = 0, n1 = 0, n2 = 0, temp = 0, temp1 = 0;
+			Console.Write("Enter a Number: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			n1 = n2 = n;
+			temp1 = n;
+			while (n > 0)
+			{
+				n = n / 10;
+				temp++;
+			}
+			while (n1 != 0)
+			{
+				r = n1 % 10;
+				s = s + (int)Math.Pow(r, temp);
+				n1 = n1 / 10;  
+			}
+			if (s == temp1)
+				Console.WriteLine(n2 + " is an Armstrong Number");
+			else
+				Console.WriteLine(n2 + " is not an Armstrong Number");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ fibonacci.cs
@@ -0,0 +1,31 @@
+﻿//Q-1 Write a program to check whether given number is in Fibonacci series or not.
+
+using System;
+
+namespace Lab_18_09_2020
+{
+    class fibonacci
+    {
+        static void Main(string[] args)
+        {
+            long n1 = 0, n2 = 1, n3 = 0, temp = 0;
+            int f = 0;
+            Console.Write("Enter Number to Check: ");
+            long n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            for (long i = 0; i <= n; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                if (n == n3)
+                    f = 1;
+            }
+            if (f == 1)
+                Console.WriteLine(temp + " is in Fibonacci Series");
+            else
+                Console.WriteLine(temp + " is not in Fibonacci Series");
+            Console.ReadKey();
+        }
+    }
+}
