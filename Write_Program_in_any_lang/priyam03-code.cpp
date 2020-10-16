b4b5e6f1680206f1d0140525d4e1f401925b92a0
priyam03-code <priyamyash03@gmail.com>
2020-10-17 00:45:50 +0530

    'hello'

---
+++ Write_Program_in_any_lang/priyam03-code.cpp
@@ -0,0 +1,15 @@
+### Write  Hello World Program  in any language
+    - fork this repo 
+    - Add "Hello World" program in your favorite language.
+
+     - your file name must be your username (Example)
+        Name: Tanvi.cpp
+        
+#include<iostream>
+using namespace std;
+int main()
+{
+        cout<<"***Hello World***<<endl;
+        return 0;
+}
+                                                      
