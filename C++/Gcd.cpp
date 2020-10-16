fb80bc1e6344aa070e491411f877191f329b0e7e
satyampgt4 <59261812+satyampgt4@users.noreply.github.com>
2020-10-16 12:37:04 +0530

    GCD OF TWO NUMBER

---
+++ C++/Gcd.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream>
+using namespace std;
+// Recursive function for gcd of a and b
+int gcd(int a, int b)
+{
+    // Everything divides 0 
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+    // base case
+    if (a == b)
+        return a;
+  
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+  
+
+int main()
+{
+    int a = 98, b = 56;
+    cout<<"GCD of "<<a<<" and "<<b<<" is "<<gcd(a, b);
+    return 0;
+}
