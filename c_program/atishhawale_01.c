abefeb2506c7ca1149e9253040f0b71217b7a53b
atishhawale <70513927+atishhawale@users.noreply.github.com>
2020-10-16 14:38:17 +0530

    Create atishhawale_01.c

---
+++ c_program/atishhawale_01.c
@@ -0,0 +1,21 @@
+/*       
+          Name: Atish Hawale
+          Program Name : Find ASCII Value of Character    (as per list)                           
+                                    
+*/
+
+/*###########################Find ASCII Value of Character################################## */
+
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
+/*###########################Program End################################## */
