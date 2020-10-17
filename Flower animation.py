e11c9215f9dbdaa25102688f32d97e7999d30324
Sambhav-Jindal-github <72639836+Sambhav-Jindal-github@users.noreply.github.com>
2020-10-17 11:56:44 +0530

    Add files via upload

---
+++ Flower animation.py
@@ -0,0 +1,24 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.pencolor('white')
+t.speed(-50)
+c = 0
+d = 0
+
+while True:
+    for i in range(4):
+        t.forward(80)
+        t.right(90)
+    t.right(5)
+    c += 1
+    if c >= 390/5:
+       t.forward(50)
+       c = 0
+       d += 1
+       if d >= 12:
+            break
+t.hideturtle()
+turtle.done()
