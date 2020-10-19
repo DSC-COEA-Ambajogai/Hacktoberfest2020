f7f8a6947290afe7229b2c8343342dd26a784a51
shreykpatnaik <shreykpatnaik@gmail.com>
2020-10-20 03:51:52 +0530

    Added shreykpatnaik_01.c

---
+++ c_program/shreykpatnaik_01.c
@@ -0,0 +1,17 @@
+/*       
+          Name: Shrey Patnaik
+          Program Name : Find ASCII Value of Character                          
+*/
+
+/*###########################Program start################################## */
+
+#include <stdio.h>
+
+int main(){
+char c;
+printf("Please enter a character: ");
+scanf("%c", &c); 
+printf("ASCII value of %c is %d\n", c, c);
+return 0;
+}
+/*###########################Program End################################## */
