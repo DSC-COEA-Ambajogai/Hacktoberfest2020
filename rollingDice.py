1abacbb6352df8f7d98b2cc6f4cf90809cc15a16
madhuri0201 <73581827+madhuri0201@users.noreply.github.com>
2020-10-28 14:54:14 +0530

    updated rollingDice.py

--- rollingDice.py
+++ rollingDice.py
@@ -9,3 +9,16 @@ def roll(min, max):
             break
 
 roll(1, 6)
+
+
+
+import random
+min = 1
+max = 6
+roll_again="yes"
+while roll_again =="yes" or roll_again =="y":
+    print("Rolling the dices...")
+    print("the values are...")
+    print(random.randint(min,max))
+    print(random.randint(min,max))
+    roll_again = input("roll the dices again")
