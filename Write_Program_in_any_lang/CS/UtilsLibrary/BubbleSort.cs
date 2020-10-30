e861a92a10ad643ed6bc60edff859f2ca0973e85
omvf <61068260+omvf@users.noreply.github.com>
2020-10-30 20:42:34 +0000

    feat: CSharp BubbleSort

---
+++ Write_Program_in_any_lang/CS/UtilsLibrary/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilsLibrary
+{
+    public class BubbleSort
+    {
+        public static void GetBubbleSort(int[] inArg)
+        {
+            var itemMoved = false;
+            do
+            {
+                itemMoved = false;
+                for (int i = 0; i < inArg.Length - 1; i++)
+                {
+                    if (inArg[i] > inArg[i + 1])
+                    {
+                        var lowerValue = inArg[i + 1];
+                        inArg[i + 1] = inArg[i];
+                        inArg[i] = lowerValue;
+                        itemMoved = true;
+                    }
+                }
+            } while (itemMoved);
+        }
+    }
+}
