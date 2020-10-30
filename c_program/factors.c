52f4294c4e0281294aa6b8bb76a1d6764b08467c
Choudhari Dipti <69644901+choudharidip@users.noreply.github.com>
2020-10-30 18:38:51 +0530

    Create factors.c
    update

---
+++ c_program/factors.c
@@ -0,0 +1,23 @@
+/*       
+          Name:Dipti Choudhari
+          Program Name : Factors of a Positive Integer              
+*/
+
+/*###########################Program start################################## */
+
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Factors of %d are: ", num);
+    for (i = 1; i <= num; ++i) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
+/*###########################Program End################################## */
