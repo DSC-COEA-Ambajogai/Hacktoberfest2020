0f68fede024fbd0ce4f21d73ec6fbec5661f2269
Ian Hunter <ian.hunter@intel.com>
2021-10-01 13:51:59 +0100

    Update rollingDice.py

--- rollingDice.py
+++ rollingDice.py
@@ -4,21 +4,15 @@ def roll(min, max):
     while True: 
         print("Rolling the dices...")
         print(f"Your number is {random.randint(min, max)}")
-        answer = input("Do you want to roll the dices again? (y/n) ")
-        if answer.lower() == "n":
-            break
 
-roll(1, 6)
+        roll_again = ""
+        while roll_again.lower() not in ["y","yes","n","no"]
+            answer = input("Do you want to roll the dices again? (y/n) ")
+            if answer.lower() in ["n", "no"]:
+                print("End.")
+                break
+            if answer.lower() in ["y", "yes"]:
+                continue
 
-
-
-import random
-min = 1
-max = 6
-roll_again="yes"
-while roll_again =="yes" or roll_again =="y":
-    print("Rolling the dices...")
-    print("the values are...")
-    print(random.randint(min,max))
-    print(random.randint(min,max))
-    roll_again = input("roll the dices again")
+if __name__ == "__main__":
+    roll(1, 6)
